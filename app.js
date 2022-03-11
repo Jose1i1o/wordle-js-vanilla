a1a2d14120679f2b149df1666fbce77cdf99093d
Jose1i1o <guadix88@gmail.com>
2022-03-11 15:16:19 +0100

    feat(check words, color cells and display messages)

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const gridDisplay = document.querySelector('.game-grid');
 const keyboard = document.querySelector('.keyboard-container');
+const messageContainer = document.querySelector('.message-container');
 
-const wordle = "magic";
-let currentCel = 0;
-let celColumn = 0;
+const secretWord = "casco";
+let thisRow = 0;
+let thisColumn = 0;
 
 
 
@@ -11,37 +12,42 @@ const keyboardKeys = [
     {
         key: 'Q',
         keyCode: 81,
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+        audio: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+    },
+    {
+        key: 'O',
+        keyCode: 79,
+        audio: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
     },
     {
         key: 'W',
         keyCode: 87,
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+        audio: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
     },
     {
         key: 'E',
         keyCode: 69,
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+        audio: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
     },
     {
         key: 'A',
         keyCode: 65,
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+        audio: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
     },
     {
         key: 'S',
         keyCode: 83,
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+        audio: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
     },
     {
         key: 'D',
         keyCode: 68,
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+        audio: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
     },
     {
         key: 'Z',
         keyCode: 90,
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+        audio: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
     },
     {
         key: 'X',
@@ -51,7 +57,7 @@ const keyboardKeys = [
     {
         key: 'C',
         keyCode: 67,
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+        audio: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
     },
     {
         key: 'Enter',
@@ -76,7 +82,7 @@ const rowAttempts = [
 
 rowAttempts.forEach((rowAttempt, rowAttemptIndex) => {
     const row = document.createElement('div');
-    row.setAttribute('id', 'row-' + rowAttempt);
+    row.setAttribute('id', 'row-' + rowAttemptIndex);
     rowAttempt.forEach((attempt, attemptIndex) => {
         const cell = document .createElement('div');
         cell.classList.add('cell');
@@ -103,39 +109,88 @@ keyboardKeys.forEach(key => {
 }
 );
 
-const handleClick = (key) => {
+const handleClick = function(key) {
     const keyAudio = document.getElementById(key).getAttribute('data-key-audio');
     const audio = new Audio(keyAudio);
+    audio.play();
     if (key === 'Delete') {
         deleteLetter();
         console.log(rowAttempts);
         return;
     }
     if (key === 'Enter') {
-        console.log('check row');
+        console.log('check row' + checkMyRow());
+        console.log(rowAttempts);
         return;
     }
     addLetter(key);
-    audio.play();
 };
 
-const addLetter = (key) => {
-    if (currentCel < 6 && celColumn < 5) {
-        const cell = document.getElementById('rowAttempt-' + currentCel + '-attempt-' + celColumn);
+const addLetter = function(key) {
+    if (thisRow < 6 && thisColumn < 5) {
+        const cell = document.getElementById('rowAttempt-' + thisRow + '-attempt-' + thisColumn);
         cell.textContent = key;
-        rowAttempts[currentCel][celColumn] = key;
+        rowAttempts[thisRow][thisColumn] = key;
         cell.setAttribute('data', key);
-        celColumn++;
+        thisColumn++;
     }
 }
 
-
-const deleteLetter = () => {
-    if (celColumn > 0) {
-        celColumn--;
-        const cell = document.getElementById('rowAttempt-' + currentCel + '-attempt-' + celColumn);
+const deleteLetter = function() {
+    if (thisColumn > 0) {
+        thisColumn--;
+        const cell = document.getElementById('rowAttempt-' + thisRow + '-attempt-' + thisColumn);
         cell.textContent = '';
-        rowAttempts[currentCel][celColumn] = '';
+        rowAttempts[thisRow][thisColumn] = '';
         cell.setAttribute('data', '');
     }
 }
+
+const checkMyRow = function() {
+    const myAttempt = rowAttempts[thisRow].join('').toLocaleLowerCase();
+    if (thisColumn === 5) {
+        colorCells();
+        if (myAttempt === secretWord) {
+            displayMessage('You win!');
+            return;
+        } else if (myAttempt !== secretWord && thisRow < 5) {
+            thisRow++;
+            thisColumn = 0
+            displayMessage('Try again!');
+            return;
+        } else if (myAttempt !== secretWord && thisRow >= 5) {
+            displayMessage('Game Over!');
+            return;
+        }
+    }
+}
+
+const displayMessage = function(message) {
+    const messageText = document.createElement('div');
+    messageText.textContent = message;
+    messageText.classList.add('show-message');
+    messageContainer.appendChild(messageText);
+    setTimeout(() => {
+        messageContainer.removeChild(messageText);
+    }, 3000);
+}
+
+const colorCells = function(color) {
+    const cells = document.querySelector('#row-' + thisRow).childNodes;
+    cells.forEach((cell, index) => {
+        const cellAttribute = cell.getAttribute('data').toLocaleLowerCase();
+        console.log(cellAttribute);
+        if (cellAttribute === secretWord[index]) {
+            cell.classList.add('correct-position');
+        } else if (secretWord.includes(cellAttribute)) {
+            cell.classList.add('wrong-position');
+        } else {
+            cell.classList.add('wrong-letter');
+        }
+    });
+}
+
+
+    //   .correct-position
+    //   .wrong-position
+    //   .wrong-letter
